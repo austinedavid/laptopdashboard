97dfde21e5f7c5cbee13f20c6929fbeb672b3411
david <austinedavid96@gmail.com>
2022-10-03 12:35:30 +0100

    reupdating my stripe payment, which i added from the second file of the laptop

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ dotenv.config()
 const cors = require('cors')
 const userRoute = require('./routes/user')
 const laptopRoute = require('./routes/laptops')
-
+const paymentRoute = require('./routes/stripe')
 // connecting to mongo DB
 const connectingDB = async()=>{
     try {
@@ -28,6 +28,7 @@ app.use(cors())
 // our routes below here
 app.use('/api', userRoute)
 app.use('/api', laptopRoute)
+app.use('/api', paymentRoute)
 // app listening below
 app.listen(PORT, ()=>{
     console.log(`app running on port: ${PORT}`)
